8ff6bea4b39a0a780b05675a8195db8926adbf81
Alex Velez <velezllaquealex@gmail.com>
2024-04-19 01:01:05 -0500

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  site: 'https://gdgecuador.github.io',
+  base: 'GirlsWomenInTech'
 });
