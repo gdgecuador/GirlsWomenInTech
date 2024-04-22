c9f164bf2171e35c486976afcb22b9b755109e94
AlexVelezLl <velezllaquealex@gmail.com>
2024-04-21 22:06:34 -0500

    Fix typo

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,9 @@
 
 export { speakers } from './speakers';
 export { schedule } from './schedule';
+
+import type { Speaker } from '../types';
+
+export const getModalId = (speaker: Speaker) => {
+  return 'modal-' + speaker.name.split(' ').join('-');
+};
