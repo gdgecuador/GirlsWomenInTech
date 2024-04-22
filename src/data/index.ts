5c805a3b0fd9e3a1e21a6b34bde56be9640a86ab
AlexVelezLl <velezllaquealex@gmail.com>
2024-04-21 23:15:28 -0500

    Add speakers modal

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,7 +2,7 @@
 export { speakers } from './speakers';
 export { schedule } from './schedule';
 
-export type { Speaker } from '../types';
+import type { Speaker } from '../types';
 
 export const getModalId = (speaker: Speaker) => {
   return 'modal-' + speaker.name.split(' ').join('-');
